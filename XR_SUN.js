f96a2b3b0246a9de7d8a78a491def57badc23703
Xiaoru Sun <xiaorusun@Xiaorus-MacBook-Air.local>
2023-11-24 23:00:57 +0000

    add eventlistener on button aming to display mroe content by clicking the button

--- XR_SUN.js
+++ XR_SUN.js
@@ -1,10 +1,13 @@
-let btn = document.querySelector(".btn");
+let btn = document.querySelector(".reveal-btn");
 const hiddenContent = document.querySelector(".hidden-content")
+
 function revealContent(){
     if (hiddenContent.classList.contains("reveal-btn")){
         hiddenContent.classList.remove("reveal-btn");
+        console.log(hiddenContent.classList)
     }else{
-        hiddenContent.classList.add("reveal-btn");
+        hiddenContent.classList.add("reveal-btn")
+        console.log(hiddenContent.classList)
     }
 }
 
